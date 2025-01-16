ee784b1909d21689d2622b44dc8beae3bfbcd50f
Sadman Sajid <sadman-sajid@khan.tail26ac73.ts.net>
2025-01-16 19:35:38 +0600

    updated driver code

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
     const handleLogin = async () => {
         try {
-            const backendUrl = process.env.BACK_END_URL || 'http://172.16.2.192:3000';
+            const backendUrl = process.env.BACK_END_URL;
             const response = await axios.post(`${backendUrl}/login`, {
                 username,
                 password
